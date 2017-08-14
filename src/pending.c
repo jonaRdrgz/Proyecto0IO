45d4f6e4fc8891f94919677173bd072ace1377b5
jonaRdrgz <jonastu.9@gmail.com>
2017-08-13 22:03:39 -0600

    actualizacion pending

--- src/pending.c
+++ src/pending.c
@@ -2,25 +2,32 @@
 #include <string.h>
 
 int main() {
-    GtkBuilder      *myBuilder; 
+    GtkBuilder      *myBuilder;
     GtkWidget       *myWinPending;
- 
+
     gtk_init(NULL, NULL);
- 
+
     myBuilder = gtk_builder_new();
-    gtk_builder_add_from_file (myBuilder, "glade/pending.glade", NULL);
- 
-    myWinPending = GTK_WIDGET(gtk_builder_get_object(myBuilder, "pending"));
+    GError *err = NULL; /* It is mandatory to initialize to NULL */
+    if(0 ==   gtk_builder_add_from_file (myBuilder, "glade/pending.glade", &err))
+    {
+        /* Print out the error. You can use GLib's message logging */
+        fprintf(stderr, "Error adding build from file. Error: %s\n", err->message);
+        /* Your error handling code goes here */
+    }
+
+
+    myWinPending = GTK_WIDGET(gtk_builder_get_object(myBuilder, "win_pending"));
     gtk_builder_connect_signals(myBuilder, NULL);
 
     g_object_unref(myBuilder);
- 
-    gtk_widget_show(myWinPending);                
+
+    gtk_widget_show(myWinPending);
     gtk_main();
- 
+
     return 0;
 }
- 
+
 // called when window is closed
 void pending_destroy() {
     gtk_main_quit();
