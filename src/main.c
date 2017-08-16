d9e1f91f2707d7fb0c73b20b4be0ec058704fc5b
izcarmt95 <izcarmt95@gmail.com>
2017-08-16 00:14:20 -0600

    El OK del pending no es un boton ocupo se quite ese

--- src/main.c
+++ src/main.c
@@ -9,6 +9,24 @@ void display_pending() {
   system("./pending &"); 
 }
 
+void myCSS(void){
+
+    GtkCssProvider *provider;
+    GdkDisplay *display;
+    GdkScreen *screen;
+
+    provider = gtk_css_provider_new ();
+    display = gdk_display_get_default ();
+    screen = gdk_display_get_default_screen (display);
+    gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    const gchar *myCssFile = "myStyle.css";
+    GError *error = 0;
+
+    gtk_css_provider_load_from_file(provider, g_file_new_for_path(myCssFile), &error);
+    g_object_unref (provider);
+}
+
 void on_window_main_destroy()
 {
     gtk_main_quit();
@@ -33,15 +51,19 @@ void run_program5(){
 int main(int argc, char *argv[])
 {
     GtkBuilder      *myBuilder; 
-    GtkWidget       *window;
- 
+    GtkWidget       *window, *button1;
+ 	
+
     gtk_init(&argc, &argv);
+    myCSS();
  
     myBuilder = gtk_builder_new();
     gtk_builder_add_from_file (myBuilder, "glade/window_main.glade", NULL);
  
     window = GTK_WIDGET(gtk_builder_get_object(myBuilder, "window_main"));
     gtk_builder_connect_signals(myBuilder, NULL);
+
+  
     
     g_object_unref(myBuilder);
  
--- src/pending.c
+++ src/pending.c
@@ -1,13 +1,36 @@
 #include <gtk/gtk.h>
 #include <string.h>
 
+
+
+void myCSS(void){
+
+    GtkCssProvider *provider;
+    GdkDisplay *display;
+    GdkScreen *screen;
+
+    provider = gtk_css_provider_new ();
+    display = gdk_display_get_default ();
+    screen = gdk_display_get_default_screen (display);
+    gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    const gchar *myCssFile = "myStyle.css";
+    GError *error = 0;
+
+    gtk_css_provider_load_from_file(provider, g_file_new_for_path(myCssFile), &error);
+    g_object_unref (provider);
+}
+
+
 int main() {
     GtkBuilder      *myBuilder;
     GtkWidget       *myWinPending;
     GtkWidget *parent;
 
-
+   
     gtk_init(NULL, NULL);
+     myCSS();
+
     parent = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
     myBuilder = gtk_builder_new();
